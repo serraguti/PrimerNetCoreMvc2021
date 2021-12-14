3f0286f12bd39d7b27f70a8ac1affe9ad7976529
serraguti <pacoserranox@gmail.com>
2021-12-14 14:22:16 +0100

    Add project files.

---
+++ PrimerNetCoreMvc/Controllers/InformacionController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimerNetCoreMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerNetCoreMvc.Controllers
+{
+    public class InformacionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ControladorVista()
+        {
+            //ViewData["Nombre"] = "Alumno";
+            //ViewData["Edad"] = 28;
+
+            Persona person = new Persona();
+            person.Nombre = "Persona Model";
+            person.Edad = 44;
+            person.Email = "personamodel@gmail.com";
+            return View(person);
+        }
+
+        public IActionResult VistaControllerGet(string app, int version)
+        {
+            ViewData["DATOS"] = app + ", Versión: " + version;
+            return View();
+        }
+    }
+}
---
+++ PrimerNetCoreMvc/Controllers/OtroController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerNetCoreMvc.Controllers
+{
+    public class OtroController : Controller
+    {
+        public IActionResult Indexa()
+        {
+            return View();
+        }
+    }
+}
